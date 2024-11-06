f0e82996ea2f236553f0b246a36d6b33baa02660
hoangst <55847465+hoangst-wolffun@users.noreply.github.com>
2024-11-06 16:14:27 +0700

    Update FirebaseManager.AppCheck.cs

--- Example/Assets/Example/Scripts/Firebase/FirebaseManager.AppCheck.cs
+++ Example/Assets/Example/Scripts/Firebase/FirebaseManager.AppCheck.cs
@@ -10,23 +10,15 @@ public partial class FirebaseManager
 {
     public async void GetAppCheck()
     {
-#if (UNITY_ANDROID || UNITY_IOS) && !UNITY_EDITOR
+#if (UNITY_ANDROID || UNITY_IOS)
         CommonLog.Log("Start GetAppCheckTokenAsync");
-        string log = "GetAppCheckTokenAsync";
         try
         {
             var appCheck = FirebaseAppCheck.DefaultInstance;
             if (appCheck != null)
             {
-                CommonLog.Log("AppCheck default instance not null");
                 var appCheckToken = await appCheck.GetAppCheckTokenAsync(false);
-                log = "Start GetAppCheckTokenAsync success for default instance " +
-                      appCheckToken.Token;
                 ThetanSDKManager.Instance.SetAppCheckToken(appCheckToken.Token);
-            }
-            else
-            {
-                log = "AppCheck instance null";
             }
         }
         catch (AggregateException e)
